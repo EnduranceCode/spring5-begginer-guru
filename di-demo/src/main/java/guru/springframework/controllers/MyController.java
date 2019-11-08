de730dc50d95e482966d0eeaa058c63e0d9dabf1
Ricardo do Canto <endurancecode@gmail.com>
2019-11-08 18:41:23 +0000

    DID: Use @Primary annotation to set the Dependency Injection

--- di-demo/src/main/java/guru/springframework/controllers/MyController.java
+++ di-demo/src/main/java/guru/springframework/controllers/MyController.java
@@ -2,12 +2,21 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.services.GreetingService;
+
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        super();
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
         System.out.println("Hello World!");
-        
-        return "hello";
+
+        return greetingService.sayGreeting();
     }
 }
