791529c2355555b2134ebcf5ed1947500a7716c8
Ricardo do Canto <endurancecode@gmail.com>
2019-11-11 09:39:02 +0000

    DID: Use Spring Profiles

--- di-demo/src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ di-demo/src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
-    public static final String HELLO_GURUS = "Hello Gurus!";
+    public static final String HELLO_GURUS = "Hello Gurus! - I was injected by the property.";
 
     @Override
     public String sayGreeting() {
