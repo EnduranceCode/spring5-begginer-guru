8e9bd4ade56c314283621b7af30a628d573a0a72
Ricardo do Canto <endurancecode@gmail.com>
2019-11-26 14:03:45 +0000

    SPC: List all Owners on Owners index page
    closes https://github.com/EnduranceCode/sfg-pet-clinic/issues/16

--- sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,15 +1,30 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping(value = "/owners")
 public class OwnerController {
+    
+    private OwnerService ownerService;
+    
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        super();
+        this.ownerService = ownerService;
+    }
 
     @GetMapping(value = { "", "/", "/index", "index.html" })
-    public String listOwners() {
+    public String listOwners(Model model) {
+        
+        model.addAttribute("owners", ownerService.findAll());
+        
         /* Returns the path and name of the desired view file */
         return "/owners/index";
     }
