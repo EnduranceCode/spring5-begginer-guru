a562a8a26ba88f59a780a3314ff5290a614f85d1
Ricardo do Canto <endurancecode@gmail.com>
2019-11-26 15:40:58 +0000

    SPC: Update Map implementation to manage setting of ID property
    closes https://github.com/EnduranceCode/sfg-pet-clinic/issues/23

--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,17 +1,31 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, N> {
+import guru.springframework.sfgpetclinic.model.BaseEntity;
 
-    protected Map<N, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, N extends Long> {
 
-    T save(N id, T object) {
+    protected Map<Long, T> map = new HashMap<>();
 
-        map.put(id, object);
+    T save(T object) {
+
+        if (object != null) {
+            if (object.getId() == null) {
+
+                object.setId(getNextID());
+            }
+        } else {
+            
+            throw new RuntimeException("Object cannot be null");
+        }
+
+        map.put(object.getId(), object);
 
         return object;
     }
@@ -30,5 +44,20 @@ public abstract class AbstractMapService<T, N> {
 
     void deleteByID(N id) {
         map.remove(id);
+    }
+
+    private Long getNextID() {
+
+        Long nextId = null;
+
+        try {
+
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
--- sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic/sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -26,13 +26,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner ownerOne = new Owner();
-        ownerOne.setId(1L);
         ownerOne.setFirstName("Michael");
         ownerOne.setLastName("Weston");
         ownerService.save(ownerOne);
 
         Owner ownerTwo = new Owner();
-        ownerTwo.setId(2L);
         ownerTwo.setFirstName("Fiona");
         ownerTwo.setLastName("Glenanne");
         ownerService.save(ownerTwo);
@@ -40,13 +38,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
         
         Vet vetOne = new Vet();
-        vetOne.setId(1L);
         vetOne.setFirstName("Sam");
         vetOne.setLastName("Axe");
         vetService.save(vetOne);
         
         Vet vetTwo = new Vet();
-        vetTwo.setId(2L);
         vetTwo.setFirstName("Jessie");
         vetTwo.setLastName("Porter");
         vetService.save(vetTwo);
