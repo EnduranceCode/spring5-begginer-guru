da9b34e338b52daac6dd96082741d0eb522ddaa0
Ricardo do Canto <endurancecode@gmail.com>
2022-01-03 17:21:35 +0000

    SRA: Add Spring Data Repositories

---
+++ spring5-recipe-app/src/main/java/guru/springframework/recipe/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.recipe.repositories;
+
+import guru.springframework.recipe.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+}
---
+++ spring5-recipe-app/src/main/java/guru/springframework/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.recipe.repositories;
+
+import guru.springframework.recipe.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
