51c8f63389df35be2f7577162bf9ea7ad56924f5
Ricardo do Canto <endurancecode@gmail.com>
2022-01-03 18:59:36 +0000

    SRA: Use Spring Data JPA Query Methods

--- spring5-recipe-app/src/main/java/guru/springframework/recipe/controllers/IndexController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/recipe/controllers/IndexController.java
@@ -1,13 +1,36 @@
 package guru.springframework.recipe.controllers;
 
+import guru.springframework.recipe.domain.Category;
+import guru.springframework.recipe.domain.UnitOfMeasure;
+import guru.springframework.recipe.repositories.CategoryRepository;
+import guru.springframework.recipe.repositories.UnitOfMeasureRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping(value = { "", "/", "/index", "/index.html" })
+    CategoryRepository categoryRepository;
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(
+            CategoryRepository categoryRepository,
+            UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @GetMapping(value = {"", "/", "/index", "/index.html"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription(
+                "Teaspoon");
+
+        System.out.println("Category ID is " + categoryOptional.get().getId());
+        System.out.println("Unit of Measure ID is " + unitOfMeasureOptional.get().getId());
+
         /* Returns the path and name of the related view file */
         return "index";
     }
--- spring5-recipe-app/src/main/java/guru/springframework/recipe/repositories/CategoryRepository.java
+++ spring5-recipe-app/src/main/java/guru/springframework/recipe/repositories/CategoryRepository.java
@@ -1,8 +1,10 @@
 package guru.springframework.recipe.repositories;
 
 import guru.springframework.recipe.domain.Category;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+    Optional<Category> findByDescription(String decription);
 }
