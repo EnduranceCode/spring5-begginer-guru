c6ba6d0f5f6e1d98b8109c1ee2ae697fc5442307
Ricardo do Canto <endurancecode@gmail.com>
2022-01-05 14:56:19 +0000

    SPC: Convert Owner to JPA Entities
    closes https://github.com/EnduranceCode/spring5-begginer-guru/issues/36

--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,15 +1,25 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
     private static final long serialVersionUID = 6058308647075194395L;
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public Set<Pet> getPets() {
--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,14 +1,28 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
+@Entity
 public class Pet extends BaseEntity {
 
     private static final long serialVersionUID = 9052231982640691480L;
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
