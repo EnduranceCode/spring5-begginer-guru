5f7225f49caa3c5985a4773998786172fb4d42de
Ricardo do Canto <endurancecode@gmail.com>
2022-01-05 15:25:32 +0000

    SPC: Convert Vet to JPA Entities
    closes https://github.com/EnduranceCode/spring5-begginer-guru/issues/37

--- sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,12 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
     private static final long serialVersionUID = 5719734850419870704L;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
