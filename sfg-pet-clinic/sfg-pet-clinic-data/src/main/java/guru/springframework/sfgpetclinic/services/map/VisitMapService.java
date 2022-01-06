d70a47a10868c8e9bfadf20a92474d42f6140221
Ricardo do Canto <endurancecode@gmail.com>
2022-01-06 16:59:18 +0000

    SPC: Create Visit map service
    closes https://github.com/EnduranceCode/spring5-begginer-guru/issues/46

---
+++ sfg-pet-clinic/sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit findByID(Long id) {
+        return null;
+    }
+}
