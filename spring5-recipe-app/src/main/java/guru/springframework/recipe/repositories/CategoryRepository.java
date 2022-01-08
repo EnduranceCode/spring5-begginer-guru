f214b0b3646be93b67085d1536657e78a566e9e7
Ricardo do Canto <endurancecode@gmail.com>
2022-01-08 13:05:05 +0000

    SRA: Fix typo on CategoryRepository

--- spring5-recipe-app/src/main/java/guru/springframework/recipe/repositories/CategoryRepository.java
+++ spring5-recipe-app/src/main/java/guru/springframework/recipe/repositories/CategoryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
-    Optional<Category> findByDescription(String decription);
+    Optional<Category> findByDescription(String description);
 }
