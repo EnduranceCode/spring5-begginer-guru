bb5230aec284868c1d408e02e1e32e3de69a9bf0
Ricardo do Canto <endurancecode@gmail.com>
2022-07-22 09:52:49 +0100

    SRA: Create a Recipe

--- spring5-recipe-app/src/main/java/guru/springframework/recipe/controllers/RecipeController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/recipe/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.recipe.controllers;
 
+import guru.springframework.recipe.commands.RecipeCommand;
 import guru.springframework.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "/recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipe-form";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 }
